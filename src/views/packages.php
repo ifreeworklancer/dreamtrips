b4782f1f13899bc2d4057b6a32a2601db3747ecb
Oleh Talanoff <talanov.o@gmail.com>
2018-12-23 12:56:29 +0200

    #

--- src/views/packages.php
+++ src/views/packages.php
@@ -1,110 +1,142 @@
 <section id="packages">
-	<div class="container">
-		<div class="row">
-			<div class="col-12 mb-5">
-				<h2 class="section-title section-title--primary section-title--packages">
-					Пакеты участия
-				</h2>
-			</div>
-		</div>
-		<div class="row justify-content-center align-items-start">
-			<div class="col-sm-6 col-lg-5 col-xl-4">
-				<div class="packages-card">
-					<div class="card-header card-header--gold">
-						<h3 class="card-header__title">
-							Gold
-						</h3>
-					</div>
-					<div class="card-body">
-						<ul class="card-body-list list-unstyled">
-							<li class="card-body-list-item">
-								Access to DreamTrips, DreamDays, DreamNights and VolunTours
-							</li>
-							<li class="card-body-list-item">
-								DreamTrips extras: airport transfers, welcome receptions, hosts, excursions and
-								activities
-							</li>
-							<li class="card-body-list-item">
-								DreamTrips Rewards program
-							</li>
-							<li class="card-body-list-item">
-								RateShrinker
-							</li>
-							<li class="card-body-list-item">
-								Exclusive travel deals year-round
-							</li>
-							<li class="card-body-list-item">
-								Flight accident insurance3
-							</li>
-							<li class="card-body-list-item">
-								DreamTrips Exclusive Deals
-							</li>
-							<li class="card-body-list-item">
-								DreamTrips Local
-							</li>
-						</ul>
-					</div>
-					<div class="card-footer">
-						<div class="card-footer__price">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 mb-5">
+                <h2 class="section-title section-title--primary section-title--packages">
+                    Пакеты участия
+                </h2>
+            </div>
+        </div>
+        <div class="row justify-content-center align-items-start">
+            <div class="col-sm-6 col-lg-5 col-xl-4">
+                <div class="packages-card">
+                    <div class="card-header card-header--gold">
+                        <h3 class="card-header__title">
+                            Gold
+                        </h3>
+                    </div>
+                    <div class="card-body">
+                        <ul class="card-body-list list-unstyled">
+                            <li class="card-body-list-item">
+                                Доступ к DreamTrips, DreamDays, DreamNights и VolunTours
+                            </li>
+                            <li class="card-body-list-item">
+                                Дополнительные льготы DreamTrips: трансферы в аэропорты, встреча и приём гостей, гиды,
+                                экскурсии и мероприятия.
+                            </li>
+                            <li class="card-body-list-item">
+                                Программа DreamTrips Rewards и RateShrinker
+                            </li>
+                            <li class="card-body-list-item">
+                                Эксклюзивные предложения по путешествиям круглый год
+                            </li>
+                            <li class="card-body-list-item">
+                                Страхование от несчастных случаев во время полёта
+                            </li>
+                            <li class="card-body-list-item">
+                                Скидки в ресторанах и на развлечения
+                            </li>
+                            <li class="card-body-list-item">
+                                Консьерж DreamTrips
+                            </li>
+                            <li class="card-body-list-item">
+                                Персональный сайт
+                            </li>
+                            <li class="card-body-list-item">
+                                Скидки на парковку в аэропорту и лимузин-сервис
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="card-footer">
+                        <div class="card-footer__price">
                             <div class="mb-2">
-                                <small>первоначальный членский взнос</small>
+                                <small class="text-mited">первоначальный членский взнос</small>
                                 <div><strong>$279.98</strong> USD</div>
                             </div>
-							<div>
-                                <small>в месяц</small>
+                            <div class="mb-3">
+                                <small class="text-muted">в месяц</small>
                                 <div><strong>$56.99</strong> USD</div>
                             </div>
-						</div>
-						<a href="#" class="btn btn-primary open-packages-gold">
+                        </div>
+                        <a href="#" class="btn btn-primary open-packages-gold">
                             приобрести пакет
-						</a>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-6 col-lg-5 col-xl-4">
-				<div class="packages-card">
-					<div class="card-header card-header--platinum">
-						<h3 class="card-header__title">
-							Platinum
-						</h3>
-					</div>
-					<div class="card-body">
-						<ul class="card-body-list list-unstyled">
-							<li class="card-body-list-item card-body-list-item--other card-body-list-item--other__gold">
-								"Gold"+
-							</li>
-							<li class="card-body-list-item">
-								Seven-day advance booking access
-								to all DreamTrips
-							</li>
-							<li class="card-body-list-item">
-								Access to DreamTrips, DreamDays, DreamNights and VolunTours
-							</li>
-							<li class="card-body-list-item">
-								Trip-specific Upgrades on Many DreamTrips, such as resort credits, room
-								upgrades, lift tickets, green fees, spa discounts, culinary classes and more.
-							</li>
-							<li class="card-body-list-item">
-								Ability to use 50 percent more DreamTrips Points on DreamTrips
-							</li>
-						</ul>
-					</div>
-					<div class="card-footer">
-						<div class="card-footer__price">
-							<span class="font-weight-bold">$99.99</span> USD
-						</div>
-						<div class="card-footer__data">
-							per month
-						</div>
-						<a href="#" class="btn btn-primary open-packages-platinum">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-5 col-xl-4">
+                <div class="packages-card">
+                    <div class="card-header card-header--platinum">
+                        <h3 class="card-header__title">
+                            Platinum
+                        </h3>
+                    </div>
+                    <div class="card-body">
+                        <ul class="card-body-list list-unstyled">
+                            <li class="card-body-list-item">
+                                Доступ к DreamTrips, DreamDays, DreamNights и VolunTours
+                            </li>
+                            <li class="card-body-list-item">
+                                Дополнительные льготы DreamTrips: трансферы в аэропорты, встреча и приём гостей, гиды,
+                                экскурсии и мероприятия.
+                            </li>
+                            <li class="card-body-list-item">
+                                Программа DreamTrips Rewards и RateShrinker
+                            </li>
+                            <li class="card-body-list-item">
+                                Эксклюзивные предложения по путешествиям круглый год
+                            </li>
+                            <li class="card-body-list-item">
+                                Страхование от несчастных случаев во время полёта
+                            </li>
+                            <li class="card-body-list-item">
+                                Скидки в ресторанах и на развлечения
+                            </li>
+                            <li class="card-body-list-item">
+                                Консьерж DreamTrips
+                            </li>
+                            <li class="card-body-list-item">
+                                Персональный сайт
+                            </li>
+                            <li class="card-body-list-item">
+                                Скидки на парковку в аэропорту и лимузин-сервис
+                            </li>
+                            <li class="card-body-list-item">
+                                Предварительный доступ к бронированию DreamTrips
+                            </li>
+                            <li class="card-body-list-item">
+                                Доступ к эксклюзивным предложениям DreamTrips Platinum
+                            </li>
+                            <li class="card-body-list-item">
+                                Особые льготы на конкретных DreamTrips: курортные кредиты,
+                                размещение в номера более высокой категории, билеты на подъёмники, плата за гольф,
+                                скидки в спа, кулинарные мастер-классы
+                            </li>
+                            <li class="card-body-list-item">
+                                Возможность использовать на 20% больше DreamTrips Points
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="card-footer">
+                        <div class="card-footer__price">
+                            <div class="mb-2">
+                                <small class="text-mited">первоначальный членский взнос</small>
+                                <div><strong>$479.98</strong> USD</div>
+                            </div>
+                            <div class="mb-3">
+                                <small class="text-muted">в месяц</small>
+                                <div><strong>$99.99</strong> USD</div>
+                            </div>
+                        </div>
+                        <a href="#" class="btn btn-primary open-packages-platinum">
                             приобрести пакет
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="decor-line decor-line--packages">
-			<span class="decor-line__text">Пакеты</span>
-		</div>
-	</div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="decor-line decor-line--packages">
+            <span class="decor-line__text">Пакеты</span>
+        </div>
+    </div>
 </section>
