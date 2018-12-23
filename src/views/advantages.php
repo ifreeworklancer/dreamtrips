ede1c7f712415b400644c8e800ac82ccf1feea41
Oleh Talanoff <talanov.o@gmail.com>
2018-12-23 12:25:57 +0200

    #

---
+++ src/views/advantages.php
@@ -0,0 +1,83 @@
+<section id="advantages">
+	<div class="container">
+		<div class="row justify-content-around pt-5">
+			<div class="col-sm-6">
+				<div class="advantages-item">
+					<h2 class="section-title section-title--primary section-title--advantages text-primary">
+						Потеряли мотивацию развиваться и двигаться дальше? Хотите вернуть драйв в свою жизнь и бизнес, обрести эффективное окружение?
+					</h2>
+					<p>
+						Увлекательное приключение и путешествие позволят вам открыть новые краски и грани
+						удивительной жизни. Начните путешествовать, почувствуйте вкус открытий!
+					</p>
+				</div>
+			</div>
+			<div class="col-sm-5">
+				<div class="advantages-img advantages-img--right"
+					 style="background-image: url('images/content/advantages/advantages-item-1.jpg');"></div>
+			</div>
+			<div class="col-sm-5">
+				<div class="advantages-img advantages-img--left"
+					 style="background-image: url('images/content/advantages/advantages-item-2.jpg');"></div>
+			</div>
+			<div class="col-sm-6">
+				<div class="advantages-item advantages-item--light">
+					<h2 class="section-title section-title--secondary text-primary">
+						Есть потенциал увеличить доход, но вы закопались в рутине? Хотите вернуть творчество в свою деятельность, реализовать новые проекты?
+					</h2>
+					<p>
+						Надёжные специалисты организуют полное сопровождение вашего отдыха. Вы сможете
+						насладиться новыми впечатлениями без негативного окраса ненужных беспокойств и решению
+						рутинных организационных вопросов.
+					</p>
+					<p>
+						Уникальная, не имеющая аналогов в мире система заказов где можно подобрать
+						индивидуальное путешествие, спланировать маршрут, организовать посещение мест отдыха и
+						достопримечательностей.
+					</p>
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<div class="advantages-item">
+					<h2 class="section-title section-title--primary text-primary">
+						Хотите получать кайф, осуществлять всё то, что хотели, но постоянно себе отказывали? Ждать новый день, а не когда он закончится?
+					</h2>
+					<p>
+						Компания обещает Вам самую низкую цену на путешествие при этом сохраняя высокий
+						стандарт качества обслуживания и сервиса.
+					</p>
+					<p>
+						Мы гарантируем самое выгодное предложение.
+						Если найдете такой же тур дешевле у любого другого тур оператора, на те же даты, вы
+						полетите бесплатно!
+					</p>
+					<p>Можете попробовать!</p>
+				</div>
+			</div>
+			<div class="col-sm-5">
+				<div class="advantages-img advantages-img--right"
+					 style="background-image: url('images/content/advantages/advantages-item-3.jpg');"></div>
+			</div>
+			<div class="col-sm-5">
+				<div class="advantages-img advantages-img--left"
+					 style="background-image: url('images/content/advantages/advantages-item-4.jpg');"></div>
+			</div>
+			<div class="col-sm-6">
+				<div class="advantages-item advantages-item--light">
+					<h2 class="section-title section-title--secondary text-primary">
+						Вам страшно кардинально что-то менять? Находитесь на грани эмоционального выгорания от постоянных перегрузок и стресса?
+					</h2>
+					<p>
+						Совместное путешествие оставит незабываемые впечатления на всю жизнь.
+					</p>
+					<p>
+						Если у вас и у ваших есть мечта - посетить какую либо страну и насладится ее
+						достопримечательностями, но вы откладывали ее реализацию до лучших времен то самое
+						время присоединится к нам.
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="decor-line decor-line--advantages"></div>
+	</div>
+</section>
---
+++ src/views/intro.php
@@ -0,0 +1,31 @@
+<section id="intro">
+	<div class="video-container" data-src="video/dreamtrips-video.mp4"></div>
+	<div class="container h-100 d-flex flex-column justify-content-center position-relative">
+		<div class="row">
+			<div class="col-sm-6 px-3 p-sm-0">
+				<div class="intro-item">
+					<h1 class="intro-item__title">
+						Вокруг света за 2000 дней
+					</h1>
+					<div class="intro-item__description">
+						Эксклюзивное сообщество путешественников
+					</div>
+					<a href="#" class="btn btn-light open-participate">Вступить в сообщество</a>
+				</div>
+			</div>
+		</div>
+		<!-- <div class="play-video">
+			<div class="play-video__icon"></div>
+			<div class="play-video__text">смотреть видео</div>
+		</div> -->
+		<div class="scroll-down position-absolute">
+			<div class="scroll-down__text">скролл вниз</div>
+			<div class="icon mb-2 position-relative">
+				<div class="icon__line position-absolute"></div>
+			</div>
+		</div>
+		<div class="decor-line decor-line--intro">
+			<span class="decor-line__text">Вокруг света за 2000 дней</span>
+		</div>
+	</div>
+</section>
